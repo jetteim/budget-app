3c879228b6ba210442feaef59b47a19717e69b44
Maxim Lee <jetteim@gmail.com>
2017-10-25 19:10:14 +0300

    --

--- lib/telegram/input_processor.rb
+++ lib/telegram/input_processor.rb
@@ -6,19 +6,32 @@ class InputProcessor
       chat_id = message[:chat_id]
       chats[chat_id] = [] unless chats[chat_id]
       chats[chat_id] << message[:text]
+	  puts "message: #{message.inspect}".magenta
 	  message['entities'].each do |entity|
 		case entity['type']
 		when 'mention'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'hashtag'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'bot_command'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'url'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'email'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'bold'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'italic'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'code'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'pre'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'text_link'
+		  puts "entity: #{entity.inspect}".magenta
 		when 'text_mention'
+		  puts "entity: #{entity.inspect}".magenta
+		else
 		end
 	  end
     end
