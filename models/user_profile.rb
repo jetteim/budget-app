3e2f872e89b0df271de326436571b840e740076f
Maxim Lee <jetteim@gmail.com>
2017-10-25 17:17:14 +0300

    --

--- models/user_profile.rb
+++ models/user_profile.rb
@@ -3,9 +3,9 @@ class UserProfile
 
   # property <name>, <type>
   property :id, Serial
+  property :name, String
   property :avatar, String
   property :lang, String
-  property :username, String
   property :email, String
   property :facebook, String
   property :twitter, String
