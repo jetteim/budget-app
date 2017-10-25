e41b70d002b57cd4f8b0a10d450006bf5347ce6b
Maxim Lee <jetteim@gmail.com>
2017-10-25 17:00:50 +0300

    --

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -2,12 +2,9 @@ migration 1, :create_users do
   up do
     create_table :users do
       column :id, Integer, :serial => true
-      column :username, DataMapper::Property::String, :length => 255
-      column :email, DataMapper::Property::String, :length => 255
       column :password, DataMapper::Property::String, :length => 255
-      column :avatar, DataMapper::Property::String, :length => 255
-      column :lang, DataMapper::Property::String, :length => 255
       column :token, DataMapper::Property::String, :length => 255
+
     end
   end
 
--- db/migrate/002_create_user_profiles.rb
+++ db/migrate/002_create_user_profiles.rb
@@ -2,9 +2,14 @@ migration 2, :create_user_profiles do
   up do
     create_table :user_profiles do
       column :id, Integer, :serial => true
-      column :password, DataMapper::Property::String, :length => 255
       column :avatar, DataMapper::Property::String, :length => 255
       column :lang, DataMapper::Property::String, :length => 255
+      column :username, DataMapper::Property::String, :length => 255
+      column :email, DataMapper::Property::String, :length => 255
+      column :facebook, DataMapper::Property::String, :length => 255
+      column :twitter, DataMapper::Property::String, :length => 255
+      column :google, DataMapper::Property::String, :length => 255
+      column :user_id, DataMapper::Property::Integer
     end
   end
 
